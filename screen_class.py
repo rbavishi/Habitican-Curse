4cb7c92181aefe684f0dd33ba5b171aaafbb587e
Rbavishi <rohan.bavishi95@gmail.com>
2015-08-17 22:10:54 +0530

    Added wrapper for the stdscr of curses

---
+++ screen_class.py
@@ -0,0 +1,35 @@
+import curses
+import tempfile
+
+class Screen:
+
+  def __init__(self, screen):
+    self.screen=screen
+    self.tmpfile=tempfile.TemporaryFile()
+    self.stack=[]
+
+  def SaveState(self):
+    self.stack.append(self.tmpfile.tell())
+    self.screen.putwin(self.tmpfile)
+
+  def Restore(self):
+    if len(self.stack)==0:
+      return
+
+    self.tmpfile.seek(self.stack[-1])
+    self.screen=curses.getwin(self.tmpfile)
+    self.screen.refresh()
+
+  def Display(self, string, x=0, y=0):
+    self.screen.addstr(x, y, string)
+    self.screen.refresh()
+
+  def DisplayBold(self, string, x=0, y=0):
+    self.screen.addstr(x, y, string, curses.A_BOLD)
+    self.screen.refresh()
+
+  def Highlight(self, string, x=0, y=0):
+    self.screen.addstr(x, y, string, curses.color_pair(1)|curses.A_BOLD)
+    self.screen.refresh()
+
+
