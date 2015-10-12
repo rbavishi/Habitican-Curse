18a0b21120dcab1f777a5a8b7421c61fb9d36369
Rbavishi <rohan.bavishi95@gmail.com>
2015-10-12 23:20:32 +0530

    Complete overhaul to resemble a PyPI package

---
+++ habitican_curse/__init__.py
@@ -0,0 +1,18 @@
+# Standard Library Imports
+import curses
+import tempfile
+import time
+import locale
+import threading
+
+# Custom Module Imports
+
+import config as C
+from screen import Screen
+import global_objects as G
+import helper as H
+import menu as M
+import request_manager as RM
+import interface as I
+import content as CT
+import debug as DEBUG
